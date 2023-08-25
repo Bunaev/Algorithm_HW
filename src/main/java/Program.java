2bc35862a423a316415d43e63008265099c073e9
Konstantin <bunaevkonstantin@bk.ru>
2023-08-25 17:05:27 +0300

    Решение задачи

---
+++ src/main/java/Program.java
@@ -0,0 +1,16 @@
+public class Program {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.add(0);
+        System.out.println("Первоначальный связный список: ");
+        System.out.println(list);
+        System.out.println("Разворот: ");
+        list.reverse();
+        System.out.println(list);
+    }
+}
